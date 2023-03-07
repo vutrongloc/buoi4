7b116b67d7aa9d16f3fbd22f70b1d1e8be00dcab
vutrongloc <vutrongloc641112@gmail.com>
2023-03-07 19:59:04 +0700

    sua code dong 4,5,6

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(" Loc ngao ngo");
-        System.out.println(" Loc ngao ngo");
-        System.out.println(" Loc ngao ngo");
+//        System.out.println(" Loc ngao ngo");
+//        System.out.println(" Loc ngao ngo");
+//        System.out.println(" Loc ngao ngo");
     }
 }
